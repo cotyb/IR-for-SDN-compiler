f6bb3f060cb946fc490ef33d944af9d20e8abf2c
cotyb <xieleicotyb@gmail.com>
2017-01-07 16:10:10 +0800

    implement part of Merlin to SA
    in this file, i implement the Merlin statement that without rate
    constraint to SA

--- Merlin2_SA.py
+++ Merlin2_SA.py
@@ -1,6 +1,7 @@
 #coding = utf-8
 import SA
 import reg2_DFA
+import copy
 
 # Merlin abstract syntax: [s1;...;sn], fai
 # [ x : (ip.src = 192.168.1.1 and ip.dst = 192.168.1.2 and tcp.dst = 20) -> .* dpi .* ;
@@ -8,8 +9,18 @@ import reg2_DFA
 # z : (ip.src = 192.168.1.1 and ip.dst = 192.168.1.2 and tcp.dst = 80) -> .* dpi .* nat .* ],
 # max(x + y,50MB/s) and min(z,100MB/s)
 
-function = ["dpi", "nat", "count", "dpi"]
-fun_switch = {'dpi':['a', 'b'], 'nat':['c', 'd'], 'count':['e'], 'dpi':['f']}
+function = ["dpi", "nat", "count", "ftp"]
+fun_switch = {'dpi':['a', 'b'], 'nat':['c', 'd'], 'count':['e'], 'ftp':['f']}
+fun_guard_update = {'dpi':['', ''], 'nat':['', ''], 'count':['', 'rv(CNT)']}
+
+def get_switch_fun(fun_switch):
+    result = {}
+    for fun, switch in fun_switch.items():
+        for sw in switch:
+            result[sw] = fun
+    return result
+
+switch_fun = get_switch_fun(fun_switch)
 
 def statement2_dict(statement):
     if not statement:
@@ -27,7 +38,7 @@ def constraint2_dict(constraint):
     result = {}
     if not constraint:
         return
-    constraints = constraint.split("and")
+    constraints = constraint.strip().split("and")
     for const in constraints:
         const = const.strip()
         if const.startswith("max"):
@@ -48,10 +59,29 @@ def parser_policy(policy):
         statement, constraint = policy, ''
     statement = statement2_dict(statement)
     if constraint == '':
-        constraint = constraint2_dict(constraint)
-    else:
         constraint = {}
+    else:
+        constraint = constraint2_dict(constraint)
     return statement, constraint
+
+
+def change_format(nodes, edges):
+    dfa_edges = edges
+    dfa_nodes = [list(x) for x in nodes]
+    add_edge = []
+    del_edge = []
+    for edge in edges:
+        if edge[2].__contains__(','):
+            del_edge.append(edge)
+            for sw in edge[2].split(','):
+                add_edge.append([edge[0], edge[1], sw])
+    dfa_edges.extend(add_edge)
+    if len(del_edge) != 0:
+        for a in del_edge:
+            dfa_edges.remove(a)
+    # dfa_nodes and dfa_edges have meet the requirements
+    return dfa_nodes, dfa_edges
+
 
 def policy2_SA(statement, constraint):
     all_sa = []
@@ -62,17 +92,96 @@ def policy2_SA(statement, constraint):
                 functions = functions.replace(fun, '(' + '|'.join(path) + ')')
             dfa = reg2_DFA.RegexDFA(functions.strip())
             dfa.minimize_dfa()
-            dfa.draw_dfa()
+            try:
+                dfa.draw_dfa()
+            except:
+                print "the graph is in dfa"
+            # dfa._edges contains all the edges in DFA, so we can change it to SA
+            # dfa._dfa_node contains all the nodes with the right order corresponding to DFA
+            # handle dfa_nodes and dfa_edges to the syntax we want
+            dfa_nodes, dfa_edges = change_format(dfa._dfa_node, dfa._dfa)
+            start = SA.Node(dfa_nodes[0][0])
+            end = SA.Node(dfa_nodes[-1][0])
+            sa = SA.SA(start, end)
+            sa.nodes = [x[0] for x in dfa_nodes]
+
+
+            print dfa_edges
+
+            for edge in dfa_edges:
+                sa_edge_start = SA.Node(int(edge[0]))
+                sa_edge_end = SA.Node(int(edge[1]))
+                if sa_edge_start == start:
+                    if edge[2] == '.':
+                        guard = ''
+                        action = 'FWD(.)'
+                        update = ''
+                        sa.add_edge_indirect(sa_edge_start, sa_edge_start, guard, "FWD(.)", update)
+                    elif edge[2] in switch_fun.keys():
+                        guard = predict
+                        new_node = sa.generate_node()
+                        action = 'FWD(%s)' %new_node.id
+                        update = ''
+                        sa_edge = SA.Edge(sa_edge_start, new_node, guard, action, update)
+                        sa.add_edge_direct(sa_edge)
+                        new_edge_start = new_node
+                        new_edge_end = sa_edge_end
+                        guard = fun_guard_update[switch_fun[edge[2]]][0]
+                        action = "FW(%s)" %(new_edge_end.id) + " && " + switch_fun[edge[2]]
+                        if fun_guard_update[switch_fun[edge[2]]][1] == '':
+                            update = ''
+                        else:
+                            update = "%s(%s)=%s"   %(switch_fun, new_edge_start.id, fun_guard_update[edge[2]][1])
+                        sa.add_edge_indirect(new_edge_start, new_edge_end, guard, action, update)
+                    else:
+                        guard = predict
+                        action = 'FWD(%s)' %edge[2]
+                        update = ''
+                        sa_edge = SA.Edge(sa_edge_start, sa_edge_end, guard, action, update)
+                        sa.add_edge_direct(sa_edge)
+
+                else:
+                    if edge[2] == '.':
+                        guard = ''
+                        action = 'FWD(.)'
+                        update = ''
+                        sa.add_edge_indirect(sa_edge_start, sa_edge_start, guard, "FWD(.)", update)
+                    elif edge[2] in switch_fun.keys():
+                        guard = ''
+                        new_node = sa.generate_node()
+                        action = 'FWD(%s)' %new_node.id
+                        update = ''
+                        sa_edge = SA.Edge(sa_edge_start, new_node, guard, action, update)
+                        sa.add_edge_direct(sa_edge)
+                        new_edge_start = new_node
+                        new_edge_end = sa_edge_end
+                        guard = fun_guard_update[switch_fun[edge[2]]][0]
+                        action = "FW(%s)" %(new_edge_end.id) + " && " + switch_fun[edge[2]]
+                        if fun_guard_update[switch_fun[edge[2]]][1] == '':
+                            update = ''
+                        else:
+                            update = "%s(%s)=%s"   %(switch_fun, new_edge_start.id, fun_guard_update[edge[2]][1])
+                        sa.add_edge_indirect(new_edge_start, new_edge_end, guard, action, update)
+                    else:
+                        guard = ''
+                        action = 'FWD(%s)' %edge[2]
+                        update = ''
+                        sa_edge = SA.Edge(sa_edge_start, sa_edge_end, guard, action, update)
+                        sa.add_edge_direct(sa_edge)
+
+            # add a edge to end
+            sa.add_edge_indirect(end, sa.generate_node(), '', 'FWD(e)', '')
+            all_sa.append(sa)
+
 
 
 if __name__ == "__main__":
     policy ="[ x : (ip.src = 192.168.1.1 and ip.dst = 192.168.1.2 and tcp.dst = 20) -> .*dpi.*;\
     y : (ip.src = 192.168.1.1 and ip.dst = 192.168.1.2 and tcp.dst = 21) -> .* ;\
     z : (ip.src = 192.168.1.1 and ip.dst = 192.168.1.2 and tcp.dst = 80) -> .*dpi.*nat.* ],\
-    "
-    #max(x,50MB/s) and min(y,100MB/s)"
+    max(x,50MB/s) "
+    "and min(y,100MB/s)"
 
 
     statement, constraint = parser_policy(policy)
-    print statement, constraint
     policy2_SA(statement, constraint)
